2a00741ae39370b10f812540d88d4d8d4870e256
Nguyễn Dương Quốc Thái <quocthai0099@gmail.com>
2021-05-12 11:19:45 +0700

    Thai update font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,8 +16,8 @@
     <!-- Google font-->
     {{-- <link href="https://fonts.googleapis.com/css?family=Rubik:400,400i,500,500i,700,700i&display=swap"
         rel="stylesheet"> --}}
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900&display=swap"
-        rel="stylesheet">
+    {{-- <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i,900&display=swap"
+        rel="stylesheet"> --}}
         <link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@400;500;600;700;800&display=swap" rel="stylesheet">
     @include('layouts.css')
     @yield('css')
